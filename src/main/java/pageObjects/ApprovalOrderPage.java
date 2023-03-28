bbebe37d9a2f9cdaf418287c38771b854af2e7ff
Svetlana Zinovich <SSZinovich@sberbank.ru>
2023-03-28 23:43:55 +0300

    first commit

---
+++ src/main/java/pageObjects/ApprovalOrderPage.java
@@ -0,0 +1,36 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class ApprovalOrderPage {
+    private static final String PAGE_URL = "https://qa-scooter.praktikum-services.ru/";
+    private static final By BUTTON_CONFIRM_ORDER = By.xpath(".//button[text()='Да']");
+    private static final By SUCCESS_HEADER = By.xpath(".//div[text()='Заказ оформлен']");
+
+    public void clickApproveButton() {
+        driver.findElement(BUTTON_CONFIRM_ORDER).click();
+    }
+
+    private final WebDriver driver;
+    public ApprovalOrderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void open() {
+        driver.get(PAGE_URL);
+    }
+
+    public void checkIfOrderComplete() {
+        if (!driver.findElement(SUCCESS_HEADER).isDisplayed()) {
+            try {
+                throw new Exception("Заголовок \"Заказ оформлен\" не найден");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
